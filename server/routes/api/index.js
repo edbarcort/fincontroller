233d5929a73d63c72e4b6e56e43163164a3ea353
et code <edgar.topete@gmail.com>
2019-08-14 21:32:52 -0500

    v2.1.1

--- server/routes/api/index.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const finRoutes = require("./fin");
-// Book routes
-//router.use("/books", bookRoutes);
-router.use('/data', finRoutes);
-module.exports = router;
