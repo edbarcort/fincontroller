c7bd073d499ece0c7d38bec1546b6291fb67f472
et code <edgar.topete@gmail.com>
2019-08-14 22:17:20 -0500

    v2.2.4

---
+++ client/src/components/config.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyApdnSDycxsGfsYRfHk3O3yWohMy-zdU-w",
+    authDomain: "financialcontroller-ba501.firebaseapp.com",
+    databaseURL: "https://financialcontroller-ba501.firebaseio.com",
+    projectId: "financialcontroller-ba501",
+    storageBucket: "",
+    messagingSenderId: "731189241427",
+    appId: "1:731189241427:web:4fad07763b438746"
+};
+
+// Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+export default firebaseConfig;
