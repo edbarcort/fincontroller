3738ec2fa1d0cdf9b579faf9cd59e7ae1dd65a14
et code <edgar.topete@gmail.com>
2019-08-15 00:45:50 -0500

    v4.1

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ var app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: false }));
