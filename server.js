76c797611562d93a80acbcb6942d1b29b241d2ca
et code <edgar.topete@gmail.com>
2019-08-15 00:25:41 -0500

    v3.2.1

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const mongoose = require("mongoose");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: false }));
